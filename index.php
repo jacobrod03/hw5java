d751af2cfbb704f34b96a7b8547a08f4627f86d2
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-06 13:20:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<h1>Homework 5</h1>
+
+<h1>Homework 5 Jacob Rodriguez</h1>
+
+<p>The University of Oklahoma.</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 'John Doe';
+</script>
+
+
 <?php
 include "view-footer.php";
 ?>
