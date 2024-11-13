056576838f367786b5534d82edec903aaa0d0bb7
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-13 12:17:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -101,6 +101,19 @@ Chart.defaults.color = '#000';
       }
     });
   </script>
+
+import Image from 'next/image'
+ 
+export default function Page() {
+  return (
+    <Image
+      src="https://s3.amazonaws.com/my-bucket/profile.png"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+    />
+  )
+}
   
 </body>
 </html>
