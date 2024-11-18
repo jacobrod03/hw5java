1ef43d156018378823baebefe4f390b7f0de83c3
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-18 11:09:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -60,6 +60,7 @@ function myFunction() {
 <head>
   <title>Classes Chart</title>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>  <!--thirdparty javascript-->
 </head>
 <body>
   <canvas id="classesChart" width="400" height="200"></canvas>
